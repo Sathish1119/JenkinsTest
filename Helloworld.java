0014155cd94827c4d3b097e037b6f1958946c522
sathish1119 <sathishkhan90@gmail.com>
2021-07-15 20:24:35 +0530

    Test file

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+class JavaSample {
+
+    public static void main(String args[]) throws Exception {
+		System.out.println("Java program triggered using Jenkins");
+    }   
+    
+
+}
